23342ce03504c79d9a91fc89e884887cee75b95e
Debtanu Maitra <maitradebtanu@gmail.com>
2023-01-26 01:22:26 +0530

    2nd Try

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("This is a test git project!!");
+
+        System.out.println("Another try!!");
     }
 }
